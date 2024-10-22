9337c34395932a3eb95a7b1e4ed19b36ded8b6fa
Martin Crabtree <martin.crabtree@gmail.com>
2024-10-22 15:50:25 -0400

    fixed issue with hover on footer

--- jenna-frontend/src/components/Footer.jsx
+++ jenna-frontend/src/components/Footer.jsx
@@ -30,6 +30,13 @@ const StyledLine = styled.div`
 const TextContainer = styled.div`
   flex: 1;
   text-align: center;
+
+  @media (max-width: 1080px) {
+    font-size: 0.8em;
+  }
+`;
+
+const StyledLink = styled.a`
   a: {
     color: #585858;
     text-decoration: none;
@@ -37,9 +44,6 @@ const TextContainer = styled.div`
   &:hover,
   &:focus {
     color: darkgray;
-  }
-  @media (max-width: 1080px) {
-    font-size: 0.8em;
   }
 `;
 
@@ -86,7 +90,7 @@ const Footer = () => {
         {/* <a href="mailto:xxx@gmail.com ">Email</a> */}
       </TextContainer>
       <TextContainer>
-        <a href="https://www.linkedin.com">
+        <StyledLink href="https://www.linkedin.com">
           <StyledSVG
             xmlns="http://www.w3.org/2000/svg"
             data-supported-dps="24x24"
@@ -95,7 +99,7 @@ const Footer = () => {
             <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
           </StyledSVG>
           <DesktopText>Linkedin</DesktopText>
-        </a>
+        </StyledLink>
       </TextContainer>
       <TextContainer>
         <CopyText>&copy; {new Date().getFullYear()} Jenna Merrill</CopyText>
