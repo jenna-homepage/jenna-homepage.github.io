8ae9d6f0674e5ede2f47ca3937cecc09a11ab77c
Martin Jenna Dev Comp <jenna.m.github@gmail.com>
2025-02-26 15:07:05 -0500

    added abnb updated text

--- jenna-frontend/src/components/Abnb.jsx
+++ jenna-frontend/src/components/Abnb.jsx
@@ -44,17 +44,23 @@ const Abnb = () => {
     <Overlay>
     <AbnbContainer>
       <TextContainer>
-        <SectionTitle>Abnb Title</SectionTitle>
-        <SectionSubHeading>
-          Abnb Subtitle
-        </SectionSubHeading>
+        <SectionTitle>Airbnb in Maple Shade, New Jersey</SectionTitle>
         <StyledHR />
+        <SectionSubHeading>
+        Meet Your Host!
+        </SectionSubHeading>
         <StyledParagraph>
-        I’m a local real estate agent that loves to travel and create fun spaces. My husband is a software engineer and is pretty laid back. We’re very open to meeting new families or individuals to share our life hacks and stories. We’ve got two little ones 2 and 4 and love to share our place and hear about your adventures. Hope to meet you soon!
+        I’m Jenna, a local real estate agent that loves to travel and create fun spaces for others during their travels. My husband is a software engineer and is pretty laid back. We’re very open to meeting new families or individuals to share our favorite local restaurants, life hacks, and stories. We’ve got two little ones 2 and 4 and love to share our place and hear about your adventures.
+
         </StyledParagraph>
+        <SectionSubHeading>
+        Where You'll Stay:
+        </SectionSubHeading>
         <StyledParagraph>
-        [ embedded link goes here ]
+        Park your car, and check yourself in with the keypad, because this weekend you'll be staying in a sleek, modern room with a queen sized bed and private bathroom, just minutes from Main Street. This room has a workspace with a desk, a closet with shelving and drawers for your things, and a TV with a Netflix account for when you want to relax. You will also have access to shared laundry facilities, a downstairs living and dining area, and an outdoor space with a playhouse and swings. Upon request we can provide a full sized air mattress, or amenities like a pack and play or highchair to make visits with small children effortless. 
         </StyledParagraph>
+        <StyledParagraph>Hope to meet you soon!</StyledParagraph>
+        <div data-id="1252944713686371251" data-view="home" data-hide-price="true" ><a href="https://www.airbnb.com/rooms/1252944713686371251?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget">View On Airbnb</a><a href="https://www.airbnb.com/rooms/1252944713686371251?guests=1&amp;adults=1&amp;s=66&amp;source=embed_widget" rel="nofollow">Home in Maple Shade · ★4.78 · 1 bedroom · 1 bed · 1 private bath</a><script async="" src="https://www.airbnb.com/embeddable/airbnb_jssdk"></script></div>
       </TextContainer>
       <ImageContainer>
         <ProfileImage
